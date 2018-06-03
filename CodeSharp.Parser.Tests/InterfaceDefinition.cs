3e0217a8d8058584a01b3dc197277f9075180202
José Medrano <jose.medrano@xamarin.com>
2018-06-03 04:56:00 -0700

    Prototype of a parser

---
+++ CodeSharp.Parser.Tests/InterfaceDefinition.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeSharp.Parser.Tests
+{
+	class TypeScriptLoader
+	{
+		
+	}
+
+	class ModuleDefinition : BaseDefinition
+	{
+		public string Id { get; set; }
+
+		public List<BaseDefinition> Definitions { get; set; } = new List<BaseDefinition> ();
+	}
+
+	class InterfaceDefinition : BaseDefinition
+	{
+		List<BaseDefinition> Definitions { get; set; } = new List<BaseDefinition> ();
+		public List<TypeDefinition> Properties => Definitions.OfType<TypeDefinition> ().ToList ();
+		public List<MethodDefinition> Methods => Definitions.OfType<MethodDefinition> ().ToList ();
+	}
+
+	abstract class BaseDefinition
+	{
+		public string Comment { get; set; }
+	}
+
+	class EnumFieldDefinition : BaseDefinition
+	{
+		public string Name { get; set; }
+		public string Value { get; set; }
+	}
+
+	class MethodDefinition : BaseDefinition
+	{
+		public string Name { get; set; }
+
+		public bool IsNulleable { get; set; }
+		public bool IsReadOnly { get; set; }
+	}
+
+	class TypeDefinition : BaseDefinition
+	{
+		public string Name { get; set; }
+		public string FieldType { get; set; }
+		public bool IsNulleable { get; set; }
+		public bool IsReadOnly { get; set; }
+	}
+
+	class PropertyTypeDefinition : TypeDefinition
+	{
+		public string Value { get; set; } 
+	}
+
+}
---
+++ CodeSharp.Parser.Tests/MarshallingTest.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace CodeSharp.Parser.Tests
+{
+	
+
+	public static class StringExtensions
+	{
+		public static string RemoveComments (this string sender)
+		{
+			int commentStartIndex;
+			while ((commentStartIndex = sender.IndexOf ("/*")) != -1) {
+				commentStartIndex += "/*".Length;
+				var tmpString = sender.Substring (commentStartIndex);
+				var nextClosedComment = tmpString.IndexOf ("*/") + "*/".Length;
+				var firstPart = sender.Substring (0, commentStartIndex - 2);
+				var secondPart = sender.Substring (commentStartIndex + nextClosedComment);
+				sender = firstPart + secondPart;
+			}
+			return sender;
+		}
+
+		public static string RemoveTabSpaces (this string sender)
+		{
+			int startIndex = 0;
+			bool hasEmptyOrTabChar;
+			while (startIndex < sender.Length && (hasEmptyOrTabChar = (sender[startIndex] == ' ' || sender [startIndex] == '\t')) == true) {
+				startIndex++;
+			}
+
+			if (startIndex == sender.Length - 1) 
+				return "";
+
+			int endIndex = sender.Length - 1;
+			while (endIndex > startIndex + 1 && (hasEmptyOrTabChar = (sender [endIndex] == ' ' || sender [endIndex] == '\t')) == true) {
+				if (hasEmptyOrTabChar) {
+					endIndex--;
+				}
+			}
+			return sender.Substring (startIndex, endIndex - startIndex);
+		}
+
+		public static string RemoveFormat (this string sender)
+		{
+			
+
+			StringBuilder builder = new StringBuilder ();
+			foreach (var item in sender.Split (new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)) {
+				builder.AppendLine (item.Trim ().Trim ('\t'));
+			}
+			return builder.ToString ();
+		}
+	}
+
+	///Users/josemedranojimenez/CodeSharp/CodeSharp.VSCode/node_modules/vscode/vscode.d.ts
+	/// 
+	/// 
+	[TestFixture ()]
+	public class MarshallingTest
+	{
+		[Test ()]
+		public void RemoveComments ()
+		{
+			var comments = "/* ******** */this /* eeeeeee */is /**/a test".RemoveComments ();
+			Assert.AreEqual ("this is a test", comments);
+		}
+
+		[Test ()]
+		public void RemoveFormat ()
+		{
+			var comments = string.Format ("    1{0}\t2{0}\t\t3", Environment.NewLine);
+			Assert.AreEqual (string.Format ("1{0}2{0}3", Environment.NewLine), comments.RemoveFormat ());
+		}
+
+
+		[Test ()]
+		public void RemoveTabSpacs ()
+		{
+			var comments = "	 	e 	        dddd		  ".RemoveTabSpaces ();	
+			Assert.AreEqual ("e 	        dddd", comments);
+		}
+
+	/**
+	 * The version of the editor.
+	 */
+	
+
+	}
+}
